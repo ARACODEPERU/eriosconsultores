15eb5c6a643e13adeb44fa3676b906c38f57637b
José Carlos <grande_josh@outlook.com>
2025-08-06 17:05:46 -0500

    s
    s

--- resources/views/pages/contact.blade.php
+++ resources/views/pages/contact.blade.php
@@ -2,9 +2,9 @@
 
 @section('content')
 
-	
+
     <!--====== PAGE BANNER PART START ======-->
-    
+
     <section id="page-banner" class="pt-80 pb-80 bg_cover" data-overlay="8" style="background-image: url({{ asset('themes/webpage/images/page-banner-1.jpg') }})">
         <div class="container">
             <div class="row">
@@ -22,11 +22,11 @@
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
-    
+
     <!--====== PAGE BANNER PART ENDS ======-->
-    
+
     <!--====== CONTACT PART START ======-->
-    
+
     <section id="contact-page" class="pt-90 pb-120 gray-bg">
         <div class="container">
             <div class="row">
@@ -37,20 +37,21 @@
                             <h2>Mantenerse en contacto</h2>
                         </div> <!-- section title -->
                         <div class="main-form pt-45">
-                            <form id="contact-form" action="contact.php" method="post" data-toggle="validator">
+                            <form id="pageContactForm" name="contact_form" action="{{ route('apisubscriber') }}" method="post" data-toggle="validator">
+                                @csrf
                                 <div class="row">
-                                    <div class="col-md-6">
+                                    <div class="col-md-12">
                                         <div class="single-form form-group">
-                                            <input name="name" type="text" placeholder="Nombres Completos" data-error="Name is required." required="required">
+                                            <input name="full_name" type="text" placeholder="Nombres Completos" data-error="Name is required." required="required">
                                             <div class="help-block with-errors"></div>
                                         </div> <!-- single form -->
                                     </div>
-                                    <div class="col-md-6">
+                                    {{-- <div class="col-md-6">
                                         <div class="single-form form-group">
                                             <input name="subject" type="text" placeholder="DNI" data-error="Subject is required." required="required">
                                             <div class="help-block with-errors"></div>
-                                        </div> <!-- single form --> 
-                                    </div>
+                                        </div> <!-- single form -->
+                                    </div> --}}
                                     <div class="col-md-6">
                                         <div class="single-form form-group">
                                             <input name="email" type="email" placeholder="Correo Electrónico" data-error="Valid email is required." required="required">
@@ -65,16 +66,16 @@
                                     </div>
                                     <div class="col-md-12">
                                         <div class="single-form form-group">
-                                            <textarea name="message" placeholder="Escribir Mensaje" data-error="Please,leave us a message." required="required"></textarea>
+                                            <textarea id="messagePageContact" name="message" placeholder="Escribir Mensaje" data-error="Please,leave us a message." required="required"></textarea>
                                             <div class="help-block with-errors"></div>
                                         </div> <!-- single form -->
                                     </div>
                                     <p class="form-message"></p>
                                     <div class="col-md-12">
                                         <div class="single-form">
-                                            <button type="submit" class="main-btn">Enviar</button>
+                                            <button id="submitPageContactButton" type="submit" class="main-btn">Enviar</button>
                                         </div> <!-- single form -->
-                                    </div> 
+                                    </div>
                                 </div> <!-- row -->
                             </form>
                         </div> <!-- main form -->
@@ -122,7 +123,64 @@
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
-    
+    <script>
+        let form = document.getElementById('pageContactForm');
+        form.addEventListener('submit', function(e) {
+            e.preventDefault();
+
+            var formulario = document.getElementById('pageContactForm');
+            var formData = new FormData(formulario);
+
+            // Deshabilitar el botón
+            var submitButton = document.getElementById('submitPageContactButton');
+            submitButton.disabled = true;
+            submitButton.style.opacity = 0.25;
+
+            // Crear una nueva solicitud XMLHttpRequest
+            var xhr = new XMLHttpRequest();
+
+            // Configurar la solicitud POST al servidor
+            xhr.open('POST', "{{ route('apisubscriber') }}", true);
+
+            // Configurar la función de callback para manejar la respuesta
+            xhr.onload = function() {
+                // Habilitar nuevamente el botón
+                submitButton.disabled = false;
+                submitButton.style.opacity = 1;
+                if (xhr.status === 200) {
+                    var response = JSON.parse(xhr.responseText);
+                    Swal.fire({
+                        icon: 'success',
+                        title: 'Enhorabuena',
+                        text: response.message,
+                        customClass: {
+                            container: 'sweet-modal-zindex' // Clase personalizada para controlar el z-index
+                        }
+                    });
+                    formulario.reset();
+                } else if (xhr.status === 422) {
+                    var errorResponse = JSON.parse(xhr.responseText);
+                    // Maneja los errores de validación aquí, por ejemplo, mostrando los mensajes de error en algún lugar de tu página.
+                    var errorMessages = errorResponse.errors;
+                    var errorMessageContainer = document.getElementById('messagePageContact');
+                    errorMessageContainer.innerHTML = 'Errores de validación:<br>';
+                    for (var field in errorMessages) {
+                        if (errorMessages.hasOwnProperty(field)) {
+                            errorMessageContainer.innerHTML += field + ': ' + errorMessages[field].join(', ') +
+                                '<br>';
+                        }
+                    }
+                } else {
+                    console.error('Error en la solicitud: ' + xhr.status);
+                }
+
+
+            };
+
+            // Enviar la solicitud al servidor
+            xhr.send(formData);
+        });
+    </script>
     <!--====== CONTACT PART ENDS ======-->
 
 @stop
