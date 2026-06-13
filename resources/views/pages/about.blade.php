ed9efd2a5ee047bd8f41e5358274dee23a30a2df
José Carlos <grande_josh@outlook.com>
2026-06-13 10:45:10 -0500

    d

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,7 +3,7 @@
 @section('content')
 
     <!--====== PAGE BANNER PART START ======-->
-    
+
     <section id="page-banner" class="pt-80 pb-80 bg_cover" data-overlay="8" style="background-image: url({{ asset('themes/webpage/images/page-banner-1.jpg') }})">
         <div class="container">
             <div class="row">
@@ -21,11 +21,11 @@
             </div> <!-- row -->
         </div> <!-- container -->
     </section>
-    
+
     <!--====== PAGE BANNER PART ENDS ======-->
-   
+
     <!--====== ABOUT PART START ======-->
-    
+
     <section id="about-page" class="pt-70 pb-110">
         <div class="container">
             <div class="row">
@@ -36,15 +36,15 @@
                     </div> <!-- section title -->
                     <div class="about-cont">
                         <p>
-                            Somos una Compañía peruana que brinda servicios especializados en materia tributaria a las empresas peruanas, 
-                            que les permita cumplir con sus obligaciones tributarias formales y sustanciales y de esta manera evitar el 
-                            riesgo de tener observaciones en una fiscalización de SUNAT y también que les permita optimizar su carga 
+                            Somos una Compañía peruana que brinda servicios especializados en materia tributaria a las empresas peruanas,
+                            que les permita cumplir con sus obligaciones tributarias formales y sustanciales y de esta manera evitar el
+                            riesgo de tener observaciones en una fiscalización de SUNAT y también que les permita optimizar su carga
                             fiscal.
                         </p>
                         <p>
-                            Bridamos una atención personalizada a cada cliente, según la realidad de su negocio y considerando los riesgos 
-                            tributarios aplicables al sector. Asimismo, brindamos recomendaciones de control interno tributario que permitan 
-                            generar los medios probatorios durante todo el proceso operativo, los cuales permitirán levantar las 
+                            Bridamos una atención personalizada a cada cliente, según la realidad de su negocio y considerando los riesgos
+                            tributarios aplicables al sector. Asimismo, brindamos recomendaciones de control interno tributario que permitan
+                            generar los medios probatorios durante todo el proceso operativo, los cuales permitirán levantar las
                             observaciones e n una fiscalización.
                         </p>
                     </div>
@@ -53,7 +53,7 @@
                     <div class="about-image mt-50">
                         <img src="{{ asset('themes/webpage/images/about/about-2.jpg') }}" alt="About">
                     </div>  <!-- about image -->
-                </div> 
+                </div>
             </div> <!-- row -->
             <div class="about-items pt-60">
                 <div class="row justify-content-center">
@@ -62,8 +62,8 @@
                             <span>01</span>
                             <h4>Misión</h4>
                             <p>
-                                ERIOS Tax & Compliance es una empresa de Consultoría Tributaria altamente especializada que brinda servicio 
-                                de alta calidad, con un equipo con amplia experiencia en el campo tributario que permite a sus clientes 
+                                ERIOS Tax & Compliance es una empresa de Consultoría Tributaria altamente especializada que brinda servicio
+                                de alta calidad, con un equipo con amplia experiencia en el campo tributario que permite a sus clientes
                                 gestionar el riesgo tributario y crecer sostenidamente.
                             </p>
                         </div> <!-- about single -->
@@ -90,11 +90,11 @@
             </div> <!-- about items -->
         </div> <!-- container -->
     </section>
-    
+
     <!--====== ABOUT PART ENDS ======-->
 
     <!--====== COUNTER PART START ======-->
-    
+
     {{-- <div id="counter-part" class="bg_cover pt-65 pb-110" data-overlay="8" style="background-image: url(images/bg-2.jpg)">
         <div class="container">
             <div class="row">
@@ -125,11 +125,11 @@
             </div> <!-- row -->
         </div> <!-- container -->
     </div> --}}
-    
+
     <!--====== COUNTER PART ENDS ======-->
-   
+
     <!--====== TEACHERS PART START ======-->
-    
+
     {{-- <section id="teachers-part" class="pt-65 pb-120">
         <div class="container">
             <div class="row">
@@ -232,15 +232,15 @@
             </div> <!-- row -->
         </div> <!-- container -->
     </section> --}}
-    
+
     <!--====== TEACHERS PART ENDS ======-->
-   
+
     <!--====== TEASTIMONIAL PART START ======-->
     <x-testimonial />
     <!--====== TEASTIMONIAL PART ENDS ======-->
-   
+
     <!--====== PATNAR LOGO PART START ======-->
-    
+
     <div id="patnar-logo" class="pt-40 pb-80 gray-bg">
         <div class="container">
             <div class="row patnar-slide">
@@ -276,9 +276,9 @@
                 </div>
             </div> <!-- row -->
         </div> <!-- container -->
-    </div> 
-    
+    </div>
+
     <!--====== PATNAR LOGO PART ENDS ======-->
-   
+
 
 @stop
--- routes/system.php
+++ routes/system.php
@@ -1,14 +1,14 @@
 <?php
 
-use App\Http\Controllers\BankAccountController;
+//use App\Http\Controllers\BankAccountController; //revisar esta ruta da problemas
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth')->group(function () {
-    Route::post('bank/account/store', [BankAccountController::class, 'storeOrUpdate'])
-        ->name('bank-account-store');
-    Route::delete('bank/account/destroy/{id}', [BankAccountController::class, 'destroy'])
-        ->name('bank-account-destroy');
+    // Route::post('bank/account/store', [BankAccountController::class, 'storeOrUpdate']) //revisar esta ruta da problemas
+    //     ->name('bank-account-store');
+    // Route::delete('bank/account/destroy/{id}', [BankAccountController::class, 'destroy']) //revisar esta ruta da problemas
+    //     ->name('bank-account-destroy');
 
     ///users///
     Route::get('datatables/users', [UserController::class, 'getUsers'])->name('users-tables-list');
